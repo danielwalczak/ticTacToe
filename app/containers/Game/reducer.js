3441a98cc95f6ccb3b44d5d742b82728d12b9cf0
midicode <contact.walczak@gmail.com>
2016-08-17 16:13:18 +0200

    Handle setMark action by reducer

--- app/containers/Game/reducer.js
+++ app/containers/Game/reducer.js
@@ -1,4 +1,7 @@
 import { fromJS } from 'immutable';
+import {
+  SET_MARK,
+} from './constants';
 
 const initialState = fromJS({
   board: Array(9).fill(0),
@@ -6,6 +9,10 @@ const initialState = fromJS({
 
 function gameReducer(state = initialState, action) {
   switch (action.type) {
+    case SET_MARK:
+      return state
+        .updateIn(['board'], board =>
+          board.set(action.index, action.value));
     default:
       return state;
   }
--- app/containers/Game/tests/reducer.test.js
+++ app/containers/Game/tests/reducer.test.js
@@ -1,11 +1,26 @@
 import expect from 'expect';
-import gameReducer from '../reducer';
 import { fromJS } from 'immutable';
+import gameReducer from '../reducer';
+import { setMark } from '../actions';
 
 describe('gameReducer', () => {
-  it('returns the initial state', () => {
-    expect(gameReducer(undefined, {})).toEqual(fromJS({
+  let state;
+  beforeEach(() => {
+    state = fromJS({
       board: Array(9).fill(0),
-    }));
+    });
+  });
+
+  it('returns the initial state', () => {
+    expect(gameReducer(undefined, {})).toEqual(fromJS(state));
+  });
+
+  it('should handle the setMark action correctly', () => {
+    const index = 0;
+    const value = 1;
+    const expectedResult = state
+      .updateIn(['board'], board => board.set(index, value));
+
+    expect(gameReducer(state, setMark(index, value))).toEqual(expectedResult);
   });
 });
